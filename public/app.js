44f254cabd0b4353ffdd27d5523ddb87864fa0a5
Logan Grosz <logangrosz@logans-mbp.home>
2019-06-25 00:58:59 -0500

    Got a basic display for the browser working, accessing routes from the database.

--- public/app.js
+++ public/app.js
@@ -15,19 +15,50 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 
 // Display routes function
-function route_browser(){
+function route_browser() {
 
     route_browser_div = document.getElementById("route-browser");
 
     const db = firebase.firestore();
-    const routes_collection = db.collection("routes");
-    const myroute = routes_collection.doc("The Cheat");
-    
-    myroute.get()
-        .then(doc => {
-            const data = doc.data();
-            console.log(data.name);
-            console.log(data.grade);
-            console.log(data.setter);
+
+    db.collection("routes")
+        .get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data is never undefined for query snapshots
+                route_data = doc.data();
+
+                //make strings fit in the button
+                //TODO Make this more robust - measure the strrngs on each
+                //     line and see if that string will exceed the length
+                //     of the button set in the stylesheet.
+                MAX_CHAR = 16
+                name_string = elipse_truncate(route_data.name, MAX_CHAR);
+                setter_string = elipse_truncate(route_data.setter, MAX_CHAR);
+
+                //setup button
+                var button = document.createElement("button");
+                button.innerHTML = name_string
+                                   + " | V" + route_data.grade
+                                   + "<br />" + setter_string;
+                var browser = document.getElementById("route-browser");
+                browser.appendChild(button);
+                console.log(doc.id, " => ", route_data);
+            });
         })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });
+    
+}
+
+//
+// HELPER FUNCTIONS
+//
+
+function elipse_truncate(string, length) {
+    if (string.length > length) {
+        string = string.substr(0, length-3) + "...";
+    }
+    return string
 }
