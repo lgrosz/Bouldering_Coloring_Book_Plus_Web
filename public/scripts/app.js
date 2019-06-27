08ca22c7a4b6cc606c47c0c4d9d9ca7c16909d07
Logan Grosz <logangrosz@logans-mbp.home>
2019-06-27 00:23:38 -0500

    Added sample hold model. Restyled html and js files. Can now view a route by clicking on its button (no recoloring or rotation yet).

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,86 +1,128 @@
 document.addEventListener('DOMContentLoaded', event => {
-    
-    // load firebase app
-    try {
-        const app = firebase.app();
-        console.log('Firebase was loaded');
-    } catch (e) {
-        console.error(e)
-        document.getElementById('load-firebase').innerHTML = 'Firebase load was unsuccessful, see console';
-    }
 
-    // do stuff with route browser
-    route_browser();
+  // load firebase app
+  try {
+    const app = firebase.app();
+    console.log('Firebase was loaded');
+  } catch (e) {
+    console.error(e)
+    document.getElementById('load-firebase').innerHTML = 'Firebase load was unsuccessful, see console';
+  }
 
-    // do stuff with route viewer
-    route_viewer();
+  initRouteBrowser();
+  initRouteViewer();
+  drawRoute(null);
 
 });
 
-// Display routes function
-function route_browser() {
+function initRouteBrowser() {
 
-    route_browser_div = document.getElementById('route-browser');
+  routeBrowserDiv = document.getElementById('route-browser');
 
-    const db = firebase.firestore();
+  const db = firebase.firestore();
 
-    db.collection('routes')
-        .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                // doc.data is never undefined for query snapshots
-                route_data = doc.data();
+  db.collection('routes')
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data is never undefined for query snapshots
+        // doc a query document snapshot, and is "shallow"
+        routeData = doc.data();
+        MAX_CHAR = 16
+        nameString = elipseTruncate(routeData.name, MAX_CHAR);
+        setterString = elipseTruncate(routeData.setter, MAX_CHAR);
 
-                //make strings fit in the button
-                //TODO Make this more robust - measure the strrngs on each
-                //     line and see if that string will exceed the length
-                //     of the button set in the stylesheet.
-                MAX_CHAR = 16
-                name_string = elipse_truncate(route_data.name, MAX_CHAR);
-                setter_string = elipse_truncate(route_data.setter, MAX_CHAR);
+        //setup button
+        var button = document.createElement('button');
+        button.classList.add('browser-button');
+        button.innerHTML = nameString
+                           + ' | V' + routeData.grade
+                           + '<br />' + setterString;
+        var browser = document.getElementById('route-browser');
+        button.onclick = function () {
+          // reload browser
+          // draw route
+          holdCollection = db.collection('routes').doc(doc.id).collection('holds');
+          drawRoute(holdCollection);
+        }
+        browser.appendChild(button);
+        console.log(doc.id, ' => ', routeData);
+      });
+    })
+    .catch(function(error) {
+      console.log('Error getting documents: ', error);
+    });
 
-                //setup button
-                var button = document.createElement('button');
-                button.classList.add('browser-button');
-                button.innerHTML = name_string
-                                   + ' | V' + route_data.grade
-                                   + '<br />' + setter_string;
-                var browser = document.getElementById('route-browser');
-                browser.appendChild(button);
-                console.log(doc.id, ' => ', route_data);
-            });
-        })
-        .catch(function(error) {
-            console.log('Error getting documents: ', error);
-        });
-    
 }
 
 // Setup canvas function
-function route_viewer() {
-    route_viewer_div = document.getElementById('route-viewer');
-    canvas = document.getElementById('route-canvas');
-    ctx = canvas.getContext('2d');
+function initRouteViewer() {
+  routeViewerDiv = document.getElementById('route-viewer');
+  canvas = document.getElementById('route-canvas');
+  ctx = canvas.getContext('2d');
 
-    const wall_image = new Image();
-    wall_image.src = '../assets/wall_images/all.png';
-    wall_image.onload = function() {
-        // canvas needs to be the same dimensions
-        height = 0.75 * window.innerHeight
-        width = wall_image.width * height / window.innerHeight
-        canvas.width = width
-        canvas.height = height
-        ctx.drawImage(wall_image, 0, 0, width, height);
-    }
+  const wallImage = new Image();
+  wallImage.src = '../assets/wall_images/all.png';
+  wallImage.onload = function() {
+    // canvas needs to be the same dimensions
+    getViewerScale(function(scale){
+      height = scale * wallImage.height;
+      width = scale * wallImage.width;
+      canvas.width = width;
+      canvas.height = height;
+      ctx.drawImage(wallImage, 0, 0, width, height);
+    });
+  }
 }
+
+function drawRoute(holdCollection) {
+  if (holdCollection != null) {
+    holdCollection.get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          holdData = doc.data();
+          drawHold(holdData);
+        });
+      });
+  }
+}
+
+function drawHold(holdData, holdImage) {
+  //recolor
+  getViewerScale(function(scale) {
+    // scale, place, rotate
+    holdImage = new Image();
+    holdImage.src = holdData.model;
+    holdImage.onload = function() {
+      height = scale * holdImage.height;
+      width = scale * holdImage.width;
+      x = scale * holdData.x;
+      y = scale * holdData.y;
+      ctx.drawImage(holdImage, x, y, width, height);
+    }
+  });
+}
+
+function getViewerScale(callback) {
+  const wallImage = new Image();
+  wallImage.src = '../assets/wall_images/all.png'
+  wallImage.onload = function() {
+    scale = 0.75 * window.innerHeight / wallImage.height;
+    callback(scale);
+  }
+}
+
+// fuction updateBrowser(currentRoute)
+// put current route on top, grayed out
+
 
 //
 // HELPER FUNCTIONS
 //
 
-function elipse_truncate(string, length) {
-    if (string.length > length) {
-        string = string.substr(0, length-3) + '...';
-    }
-    return string
+function elipseTruncate(string, length) {
+  if (string.length > length) {
+    string = string.substr(0, length-3) + '...';
+  }
+  return string
 }
