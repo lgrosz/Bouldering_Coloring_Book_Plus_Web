3f58e4ae551831930cb09d8f369fb466062b1ccb
Logan Grosz <logangrosz@logans-mbp.home>
2019-06-29 14:26:47 -0500

    Can now add holds by double clicking (just square for now) - thanks to some other guys code I'll be modifying more later. Issue: need to not load the wall to redraw it, it makes it draw over the holds sometimes (I think, maybe it's just because they're transparent).

--- public/scripts/creator.js
+++ public/scripts/creator.js
@@ -10,85 +10,19 @@ document.addEventListener('DOMContentLoaded', event => {
   init();
 });
 
-//function initRouteCreator() {
-//  clearWall(false);
-//}
-//
-//function clearWall(clearWall=true) {
-//  canvas = document.getElementById('route-canvas');
-//  ctx = canvas.getContext('2d');
-//
-//  if (clearWall == true) {
-//    ctx.clearRect(0, 0, canvas.width, canvas.height);
-//  }
-//
-//  const wallImage = new Image();
-//  wallImage.src = '../assets/wall_images/all.png';
-//  wallImage.onload = function() {
-//    // canvas needs to be the same dimensions
-//    getCreatorScale(function(scale){
-//      height = scale * wallImage.height;
-//      width = scale * wallImage.width;
-//      canvas.width = width;
-//      canvas.height = height;
-//      ctx.drawImage(wallImage, 0, 0, width, height);
-//    });
-//  }
-//}
-//
-//function getCreatorScale(callback) {
-//  const wallImage = new Image();
-//  wallImage.src = '../assets/wall_images/all.png'
-//  wallImage.onload = function() {
-//    scale = 0.95 * window.innerHeight / wallImage.height;
-//    callback(scale);
-//  }
-//}
 
-//not my source
-// By Simon Sarris
-// www.simonsarris.com
-// my name @gmail.com
-//
-// Last update December 2011
-//
-// Free to use and distribute at will
-// So long as you are nice to people, etc
-
-// Constructor for Shape objects to hold data for all drawn objects.
-// For now they will just be defined as rectangles.
-function Shape(x, y, w, h, fill) {
-  // This is a very simple and unsafe constructor. All we're doing is checking if the values exist.
-  // "x || 0" just means "if there is a value for x, use that. Otherwise use 0."
-  // But we aren't checking anything else! We could put "Lalala" for the value of x 
-  this.x = x || 0;
-  this.y = y || 0;
-  this.w = w || 1;
-  this.h = h || 1;
-  this.fill = fill || '#AAAAAA';
+function init() {
+  wall = new WallState(document.getElementById('route-canvas'));
 }
 
-// Draws this shape to a given context
-Shape.prototype.draw = function(ctx) {
-  ctx.fillStyle = this.fill;
-  ctx.fillRect(this.x, this.y, this.w, this.h);
-}
-
-// Determine if a point is inside the shape's bounds
-Shape.prototype.contains = function(mx, my) {
-  // All we have to do is make sure the Mouse X,Y fall in the area between
-  // the shape's X and (X + Width) and its Y and (Y + Height)
-  return  (this.x <= mx) && (this.x + this.w >= mx) &&
-          (this.y <= my) && (this.y + this.h >= my);
-}
-
-function CanvasState(canvas) {
-  // **** First some setup! ****
-  
+function WallState(canvas) {
+  // setup canvas dimensions
   this.canvas = canvas;
   this.width = canvas.width;
   this.height = canvas.height;
+  this.scale = this.height / window.innerHeight;
   this.ctx = canvas.getContext('2d');
+
   // This complicates things a little but but fixes mouse co-ordinate problems
   // when there's a border or padding. See getMouse for more detail
   var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
@@ -104,37 +38,34 @@ function CanvasState(canvas) {
   this.htmlTop = html.offsetTop;
   this.htmlLeft = html.offsetLeft;
 
-  // **** Keep track of state! ****
-  
-  this.valid = false; // when set to false, the canvas will redraw everything
-  this.shapes = [];  // the collection of things to be drawn
-  this.dragging = false; // Keep track of when we are dragging
-  // the current selected object. In the future we could turn this into an array for multiple selection
-  this.selection = null;
-  this.dragoffx = 0; // See mousedown and mousemove events for explanation
+  // keep track of the state of the canvas
+  this.valid = false; //if false, canvas needs to redraw
+  this.holds = []; // keeps hold data
+  this.dragging = false; // true when user is dragging
+  this.selection = null; // the selected hold
+  this.dragoffx = 0; // drag offsets
   this.dragoffy = 0;
-  
-  // **** Then events! ****
-  
-  // This is an example of a closure!
-  // Right here "this" means the CanvasState. But we are making events on the Canvas itself,
-  // and when the events are fired on the canvas the variable "this" is going to mean the canvas!
-  // Since we still want to use this particular CanvasState in the events we have to save a reference to it.
-  // This is our reference!
-  var myState = this;
-  
-  //fixes a problem where double clicking causes text to get selected on the canvas
-  canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
-  // Up, down, and move are for dragging
+
+  // `this` will mean the canvas, not WallState. But we will use WallState
+  // for redrawing things, so here is a reference to the WallState object.
+  myState = this;
+
+  // remove text selection double click behavior
+  canvas.addEventListener('selectstart', function(e) {
+    e.preventDefault();
+    return false;
+  }, false);
+
+  // for setting state variables for mouse down
   canvas.addEventListener('mousedown', function(e) {
     var mouse = myState.getMouse(e);
     var mx = mouse.x;
     var my = mouse.y;
-    var shapes = myState.shapes;
-    var l = shapes.length;
+    var holds = myState.holds;
+    var l = holds.length;
     for (var i = l-1; i >= 0; i--) {
-      if (shapes[i].contains(mx, my)) {
-        var mySel = shapes[i];
+      if (holds[i].contains(mx, my)) {
+        var mySel = holds[i];
         // Keep track of where in the object we clicked
         // so we can move it smoothly (see mousemove)
         myState.dragoffx = mx - mySel.x;
@@ -145,30 +76,27 @@ function CanvasState(canvas) {
         return;
       }
     }
-    // havent returned means we have failed to select anything.
-    // If there was an object selected, we deselect it
-    if (myState.selection) {
-      myState.selection = null;
-      myState.valid = false; // Need to clear the old selection border
-    }
-  }, true);
+  });
+
+  // for setting state variables for mouse moving
   canvas.addEventListener('mousemove', function(e) {
-    if (myState.dragging){
+    if (myState.dragging) {
       var mouse = myState.getMouse(e);
-      // We don't want to drag the object by its top-left corner, we want to drag it
-      // from where we clicked. Thats why we saved the offset and use it here
       myState.selection.x = mouse.x - myState.dragoffx;
       myState.selection.y = mouse.y - myState.dragoffy;   
-      myState.valid = false; // Something's dragging so we must redraw
+      myState.valid = false;
     }
   }, true);
+
+  //no longer dragging
   canvas.addEventListener('mouseup', function(e) {
     myState.dragging = false;
   }, true);
-  // double click for making new shapes
+
+  // double click for adding holds
   canvas.addEventListener('dblclick', function(e) {
     var mouse = myState.getMouse(e);
-    myState.addShape(new Shape(mouse.x - 10, mouse.y - 10, 20, 20, 'rgba(0,255,0,.6)'));
+    myState.addHold(new Hold(mouse.x - 10, mouse.y - 10, 20, 20, 'rgba(0,255,0,.6)'));
   }, true);
   
   // **** Options! ****
@@ -179,38 +107,37 @@ function CanvasState(canvas) {
   setInterval(function() { myState.draw(); }, myState.interval);
 }
 
-CanvasState.prototype.addShape = function(shape) {
-  this.shapes.push(shape);
+WallState.prototype.addHold = function(hold) {
+  this.holds.push(hold);
   this.valid = false;
 }
 
-CanvasState.prototype.clear = function() {
+WallState.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.width, this.height);
 }
 
-// While draw is called as often as the INTERVAL variable demands,
-// It only ever does something if the canvas gets invalidated by our code
-CanvasState.prototype.draw = function() {
-  // if our state is invalid, redraw and validate!
+WallState.prototype.draw = function() {
+  // if our state is invalid, redraw
   if (!this.valid) {
     var ctx = this.ctx;
-    var shapes = this.shapes;
+    var holds = this.holds;
     this.clear();
     
-    // ** Add stuff you want drawn in the background all the time here **
-    
-    // draw all shapes
-    var l = shapes.length;
-    for (var i = 0; i < l; i++) {
-      var shape = shapes[i];
-      // We can skip the drawing of elements that have moved off the screen:
-      if (shape.x > this.width || shape.y > this.height ||
-          shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
-      shapes[i].draw(ctx);
+    // background
+    const wallImage = new Image();
+    wallImage.src = '../assets/wall_images/all.png';
+    wallImage.onload = function() {
+      ctx.drawImage(wallImage, 0, 0, myState.width, myState.height);
     }
     
-    // draw selection
-    // right now this is just a stroke along the edge of the selected Shape
+    // draw all holds
+    var l = holds.length;
+    for (var i = 0; i < l; i++) {
+      var hold = holds[i];
+      holds[i].draw(ctx);
+    }
+    
+    // draw selection border
     if (this.selection != null) {
       ctx.strokeStyle = this.selectionColor;
       ctx.lineWidth = this.selectionWidth;
@@ -218,16 +145,13 @@ CanvasState.prototype.draw = function() {
       ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
     }
     
-    // ** Add stuff you want drawn on top all the time here **
+    // foreground
     
     this.valid = true;
   }
 }
 
-
-// Creates an object with x and y defined, set to the mouse position relative to the state's canvas
-// If you wanna be super-correct this can be tricky, we have to worry about padding and borders
-CanvasState.prototype.getMouse = function(e) {
+WallState.prototype.getMouse = function(e) {
   var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
   
   // Compute the total offset
@@ -250,17 +174,25 @@ CanvasState.prototype.getMouse = function(e) {
   return {x: mx, y: my};
 }
 
-// If you dont want to use <body onLoad='init()'>
-// You could uncomment this init() reference and place the script reference inside the body tag
-//init();
-
-function init() {
-  var s = new CanvasState(document.getElementById('route-canvas'));
-  s.addShape(new Shape(40,40,50,50)); // The default is gray
-  s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
-  // Lets make some partially transparent
-  s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
-  s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
+function Hold(x, y, w, h, fill) {
+  // This is a very simple and unsafe constructor. All we're doing is checking if the values exist.
+  // "x || 0" just means "if there is a value for x, use that. Otherwise use 0."
+  // But we aren't checking anything else! We could put "Lalala" for the value of x 
+  this.x = x || 0;
+  this.y = y || 0;
+  this.w = w || 1;
+  this.h = h || 1;
+  this.fill = fill || '#AAAAAA';
 }
 
-// Now go make something amazing!
+// Draws this shape to a given context
+Hold.prototype.draw = function(ctx) {
+  ctx.fillStyle = this.fill;
+  ctx.fillRect(this.x, this.y, this.w, this.h);
+}
+
+// Determine if a point is inside the shape's bounds
+Hold.prototype.contains = function(mx, my) {
+  return  (this.x <= mx) && (this.x + this.w >= mx) &&
+          (this.y <= my) && (this.y + this.h >= my);
+}
