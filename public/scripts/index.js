70f4174b19c23d2f17d719673ce47f807b292f08
Logan Grosz <logangrosz@logans-mbp.home>
2019-07-14 13:44:19 -0500

    Added a home page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,31 @@
-function visitPage(url) {
-  window.location = url;
+document.addEventListener('DOMContentLoaded', event => {
+  try {
+    const app = firebase.app();
+    console.log('Firebase was loaded');
+  } catch (e) {
+    console.error(e)
+  }
+
+  console.log('hi');
+  loadImage('walls/sdsmt/all.png')
+    .then( img => {
+      console.log('hihi');
+      let centerDiv = document.getElementById('center-div');
+      img.classList.add('fitwidth');
+      centerDiv.appendChild(img);
+    });
+});
+
+function loadImage(path) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => resolve(img));
+    img.addEventListener('error', err => reject(err));
+    // get image from firebase storage
+    const storage = firebase.storage();
+    const storageRef = storage.ref().child(path);
+    storageRef.getDownloadURL().then( url => {
+      img.src = url;
+    });
+  });
 }
